66a67f6e354206637d60f0d1f761a503f089fbe5
mbondoc <maxbondoc11@gmail.com>
2024-04-10 14:37:07 -0400

    Create Negative.java

---
+++ Negative.java
@@ -0,0 +1,25 @@
+/*****************
+ * Max Bondoc
+ * 4/10/24
+ * Negative.java
+ */
+
+public class Negative{
+    public static void main(String[]args){
+        int n1 = (int) (Math.random()*7)-3;
+        int n2 = (int) (Math.random()*7)-3;
+        int n3 = (int) (Math.random()*7)-3;
+        int n4 = (int) (Math.random()*7)-3;
+        int sum = n1+n2+n3+n4;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);
+        System.out.println(sum);
+        if(sum > 0){
+            System.out.print("Not Negative");
+        }else if(sum < 0){
+            System.out.print("Negative");
+        }
+    }
+}
